5444e4c55787a995adcd6f9a870072c87425bce2
pavell016 <pavellupan229@gmail.com>
2026-03-23 16:31:22 +0100

    Initial Commit

---
+++ FlapyBrowt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlapyBrowt
+//
+//  Created by alumne on 23/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlapyBrowt/FlapyBrowtApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlapyBrowtApp.swift
+//  FlapyBrowt
+//
+//  Created by alumne on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlapyBrowtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
